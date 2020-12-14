d148b689b80f0a0008674ca59b5579c1c01efe21
GiladKay <gilad.kay236@gmail.com>
2020-12-14 21:32:39 +0200

    upgraded dialog for setting a meeting

--- app/src/main/java/com/yg/amit/StudentsActivity.java
+++ app/src/main/java/com/yg/amit/StudentsActivity.java
@@ -2,20 +2,34 @@ package com.yg.amit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class StudentsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -24,11 +38,13 @@ public class StudentsActivity extends AppCompatActivity implements View.OnClickL
     private ListView lvS;       // listView for students
     private TextView tvTitle;   // Title of the activity
     private ArrayList<Student> studentList;
-    private TextInputLayout ipDate, ipTime;
-    private TextInputEditText edtDate, edtTime;
+    private TextView tvDate, tvTime;
+    private Button btnDate, btnTime;
     private MaterialButton btnCreate;
     private TextView tvMeetCount;
     private TextView tvSName;
+
+    int tHour,tMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +59,72 @@ public class StudentsActivity extends AppCompatActivity implements View.OnClickL
         arrMeeting= new Dialog(this);
         arrMeeting.setContentView(R.layout.meeting_arrangement_dialog);
 
-        ipDate=(TextInputLayout)arrMeeting.findViewById(R.id.ipDate);
-        ipTime=(TextInputLayout)arrMeeting.findViewById(R.id.ipTime);
-
-        edtDate=(TextInputEditText)arrMeeting.findViewById(R.id.edtDate);
-        edtTime=(TextInputEditText)arrMeeting.findViewById(R.id.edtTime);
-
         tvMeetCount=(TextView)arrMeeting.findViewById(R.id.tvMeetings);
         tvSName=(TextView)arrMeeting.findViewById(R.id.tvStudentName);
+
+        tvTime=(TextView)arrMeeting.findViewById(R.id.tvTime2);
+        tvDate=(TextView)arrMeeting.findViewById(R.id.tvDate2);
+
+        DatePickerDialog.OnDateSetListener mDateSetListener;
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d("TAG", "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = day + "/" + month + "/" + year;
+                tvDate.setText(date);
+            }
+        };
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        StudentsActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+
+
+
+        tvTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog=new TimePickerDialog(
+                        StudentsActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                tHour=i;
+                                tMinute=i1;
+                                String time=i+":"+i1;
+                                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("HH:mm");
+                                try {
+                                    Date date=simpleDateFormat.parse(time);
+                                    tvTime.setText(simpleDateFormat.format(date));
+                                }
+                                catch (ParseException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                        },24,0,true);
+                timePickerDialog.updateTime(tHour,tMinute);
+                timePickerDialog.show();
+
+            }
+        });
 
         btnCreate=(MaterialButton)arrMeeting.findViewById(R.id.btnCreate);
 
@@ -77,16 +151,15 @@ public class StudentsActivity extends AppCompatActivity implements View.OnClickL
                 //TODO make sure fields are not left empty
                 tvSName.setText(studentList.get(i).getName()+" ");
                 tvMeetCount.setText(studentList.get(i).getMeetingCount()+"/2 ");
-                edtDate.setText("");
-                edtTime.setText("");
 
                 btnCreate.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                            String date = edtDate.getText().toString();
-                            String time = edtTime.getText().toString();
+                        String time=tvTime.getText().toString();
+                        String Date=tvDate.getText().toString();
                             //TODO create a meeting in a file with the date and time and sent email
                             //TODO add one to the students meeting count(update in file) and update list accordingly- lvS.setAdapter(adapter);
+                        Toast.makeText(getApplicationContext(),"time: "+time+ "Date: "+Date,Toast.LENGTH_LONG).show();
                             arrMeeting.hide();
 
                     }
