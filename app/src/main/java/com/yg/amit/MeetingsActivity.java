0e2c4d16ebdcd72f8c2a4f0d773d827478ea6243
YarinSha <yarinshattach@gmail.com>
2021-03-03 21:12:41 +0200

    fix

--- app/src/main/java/com/yg/amit/MeetingsActivity.java
+++ app/src/main/java/com/yg/amit/MeetingsActivity.java
@@ -220,7 +220,14 @@ public class MeetingsActivity extends AppCompatActivity implements View.OnClickL
                                                 }
                                             });
 
-                                            pd.dismiss();
+                                            try {
+                                                pd.dismiss();
+                                            } catch (Exception e) {
+                                                // Uh-oh, an error occurred!
+                                                Log.w("pd", "onFailure: ", e);
+                                                Toast.makeText(getApplicationContext(), "אנא השאר את המסך אנכי", Toast.LENGTH_LONG).show();
+                                                finish();
+                                            }
                                         })
                                         .addOnFailureListener(e -> {
                                             // Uh-oh, an error occurred!
